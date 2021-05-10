adbc4a341d4f6dd9780776668cae5371b27897f0
Andrew Kane <andrew@ankane.org>
2021-05-10 14:07:08 -0700

    Updated scripts

--- ann_benchmarks/algorithms/pgvector.py
+++ ann_benchmarks/algorithms/pgvector.py
@@ -16,34 +16,28 @@ class Pgvector(BaseANN):
 
         self._conn = psycopg2.connect('user=postgres dbname=vector_bench')
         self._conn.autocommit = True
+        self._cur = self._conn.cursor()
 
     def fit(self, X):
-        cur = self._conn.cursor()
-
-        cur.execute('CREATE EXTENSION IF NOT EXISTS vector')
-        cur.execute('CREATE TABLE tst (id integer, vec vector(' + str(X.shape[1]) + '))')
+        self._cur.execute('CREATE EXTENSION IF NOT EXISTS vector')
+        self._cur.execute('DROP TABLE IF EXISTS tst')
+        self._cur.execute('CREATE TABLE tst (id integer, vec vector(' + str(X.shape[1]) + '))')
 
         with open('/tmp/data.csv', 'w', newline='') as csvfile:
             csvwriter = csv.writer(csvfile)
             for i, x in enumerate(X):
                 csvwriter.writerow([i, '[' + ','.join(str(v) for v in x.tolist()) + ']'])
-        cur.execute('COPY tst (id, vec) FROM \'/tmp/data.csv\' WITH (FORMAT csv)')
+        self._cur.execute('COPY tst (id, vec) FROM \'/tmp/data.csv\' WITH (FORMAT csv)')
 
-        cur.execute('CREATE INDEX ON tst USING ivfflat (vec ' + self._opclass + ') WITH (lists = ' + str(self._lists) + ')')
-
-        cur.close()
+        self._cur.execute('CREATE INDEX ON tst USING ivfflat (vec ' + self._opclass + ') WITH (lists = ' + str(self._lists) + ')')
 
     def set_query_arguments(self, probes):
         self._probes = probes
-        cur = self._conn.cursor()
-        cur.execute('SET ivfflat.probes = %s', (str(probes),))
-        cur.close()
+        self._cur.execute('SET ivfflat.probes = %s', (str(probes),))
 
     def query(self, v, n):
-        cur = self._conn.cursor()
-        cur.execute('SELECT id FROM tst ORDER BY vec ' + self._op + ' %s LIMIT %s', ('[' + ','.join(str(v2) for v2 in v.tolist()) + ']', n))
-        res = cur.fetchall()
-        cur.close()
+        self._cur.execute('SELECT id FROM tst ORDER BY vec ' + self._op + ' %s LIMIT %s', ('[' + ','.join(str(v2) for v2 in v.tolist()) + ']', n))
+        res = self._cur.fetchall()
         return [r[0] for r in res]
 
     def __str__(self):
