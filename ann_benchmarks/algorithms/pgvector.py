3021d8649b5277055db586d6ffc8608d9e772497
Andrew Kane <andrew@ankane.org>
2023-03-25 21:17:16 -0700

    Use psycopg 3 [skip ci]

--- ann_benchmarks/algorithms/pgvector.py
+++ ann_benchmarks/algorithms/pgvector.py
@@ -1,7 +1,8 @@
 from __future__ import absolute_import
 import io
-from pgvector.psycopg2 import register_vector
-import psycopg2
+from pgvector.psycopg import register_vector
+import psycopg
+from psycopg import sql
 from ann_benchmarks.algorithms.base import BaseANN
 
 
@@ -22,40 +23,37 @@ class Pgvector(BaseANN):
         self._opclass = {'angular': 'vector_cosine_ops', 'euclidean': 'vector_l2_ops'}[metric]
         self._op = {'angular': '<=>', 'euclidean': '<->'}[metric]
         self._table = 'vectors_%s_%d' % (metric, lists)
-        self._query = 'SELECT id FROM %s ORDER BY vec %s %%s LIMIT %%s' % (self._table, self._op)
+        self._query = 'SELECT id FROM %s ORDER BY vec %s %%b LIMIT %%b' % (self._table, self._op)
 
-        self._conn = psycopg2.connect('dbname=pgvector_bench user=postgres')
+        self._conn = psycopg.connect('dbname=pgvector_bench user=postgres')
         self._conn.autocommit = True
+        self._conn.execute('CREATE EXTENSION IF NOT EXISTS vector')
+        register_vector(self._conn)
         self._cur = self._conn.cursor()
-        self._cur.execute('CREATE EXTENSION IF NOT EXISTS vector')
-        register_vector(self._cur)
 
     def fit(self, X):
         print('Creating table')
         self._cur.execute('DROP TABLE IF EXISTS %s' % (self._table,));
         self._cur.execute('CREATE TABLE %s (id integer, vec vector(%d))' % (self._table, X.shape[1]))
 
-        f = io.StringIO()
-        for i, x in enumerate(X):
-            f.write(str(i))
-            f.write('\t')
-            f.write('[' + ','.join(str(v) for v in x.tolist()) + ']')
-            f.write('\n')
-        f.seek(0)
-        self._cur.copy_from(f, self._table, columns=('id', 'vec'))
+        with self._cur.copy("COPY %s (id, vec) FROM STDIN" % (self._table,)) as copy:
+            for i, x in enumerate(X):
+                copy.write(str(i))
+                copy.write('\t')
+                copy.write('[' + ','.join(str(v) for v in x.tolist()) + ']')
+                copy.write('\n')
 
         print('Creating index')
-        self._cur.execute('SET maintenance_work_mem = %s', ('256MB',))
+        self._cur.execute("SET maintenance_work_mem = '%s'" % ('256MB',))
         self._cur.execute('CREATE INDEX ON %s USING ivfflat (vec %s) WITH (lists = %d)' % (self._table, self._opclass, self._lists))
         self._cur.execute('ANALYZE %s' % self._table)
 
     def set_query_arguments(self, probes):
         self._probes = probes
-        self._cur.execute('SET ivfflat.probes = %s', (str(probes),))
+        self._cur.execute("SET ivfflat.probes = '%s'" % (str(probes),))
 
     def query(self, v, n):
-        self._cur.execute(self._query, (v, n))
-        res = self._cur.fetchall()
+        res = self._cur.execute(self._query, (v, n)).fetchall()
         return [r[0] for r in res]
 
     def __str__(self):
